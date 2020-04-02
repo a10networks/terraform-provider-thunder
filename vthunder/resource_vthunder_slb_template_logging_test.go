df4f8dd0b9d86c0f0728b1aa55358b035234f98f
Devendra Bhat <devendra.bhat2@gmail.com>
2020-04-02 22:01:44 +0530

    updated logging test

--- vthunder/resource_vthunder_slb_template_logging_test.go
+++ vthunder/resource_vthunder_slb_template_logging_test.go
@@ -1,11 +1,12 @@
 package vthunder
 
 import (
-	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 )
 
-var TEST_MQTT_RESOURCE = `
+var TEST_LOGGING_RESOURCE = `
 resource "vthunder_slb_template_logging" "l"{
 name = "log1"
 format= "abc"
@@ -14,7 +15,7 @@ local_logging= 1
 `
 
 //Acceptance test
-func TestAccVthunderMqtt_create(t *testing.T) {
+func TestAccVthunderLogging_create(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
 			testAcctPreCheck(t)
@@ -22,7 +23,7 @@ func TestAccVthunderMqtt_create(t *testing.T) {
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: TEST_MQTT_RESOURCE,
+				Config: TEST_LOGGING_RESOURCE,
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("vthunder_slb_template_logging.logging", "name", "log1"),
 					resource.TestCheckResourceAttr("vthunder_slb_template_logging.logging", "format", "abc"),
