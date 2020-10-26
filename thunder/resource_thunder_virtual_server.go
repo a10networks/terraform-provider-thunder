5c598f299bf405302f3099fd5794b01db4e35045
arpitgupta-a10 <arpit.gupta@a10networks.com>
2020-10-26 20:10:10 +0530

    patch to stop terraform applying in case of API status fail is added in all exporter file, slb_virtual_server_port resource added, file name change from vthunder to thunder and fix in virtual_server

--- thunder/resource_thunder_virtual_server.go
+++ thunder/resource_thunder_virtual_server.go
@@ -1057,10 +1057,8 @@ func resourceVirtualServerCreate(d *schema.ResourceData, meta interface{}) error
 		logger.Println("[INFO] received formatted data from method data to vs --" + v.Name.Name + ",--" + v.Name.UUID)
 		d.SetId(name)
 		go_thunder.PostVS(client.Token, v, client.Host)
-
-		//return resourceVirtualServerRead(d, meta)
-		return nil
-	}
+		return resourceVirtualServerRead(d, meta)
+		}
 	return nil
 }
 
@@ -1100,9 +1098,8 @@ func resourceVirtualServerUpdate(d *schema.ResourceData, meta interface{}) error
 		d.SetId(name)
 		go_thunder.PutVS(client.Token, name, v, client.Host)
 
-		//return resourceVirtualServerRead(d, meta)
-		return nil
-	}
+		return resourceVirtualServerRead(d, meta)
+		}
 	return nil
 }
 
