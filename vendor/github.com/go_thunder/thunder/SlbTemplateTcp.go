aac48b018889daf4d066e314276f141084644847
arpitgupta-a10 <arpit.gupta@a10networks.com>
2021-09-08 05:30:28 +0000

    Jira key [TTP-2], Msg: Enhanced slb-template-tcp

--- vendor/github.com/go_thunder/thunder/SlbTemplateTcp.go
+++ vendor/github.com/go_thunder/thunder/SlbTemplateTcp.go
@@ -3,26 +3,39 @@ package go_thunder
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"util"
 )
 
-type TCP struct {
-	Name TCPInstance `json:"tcp"`
-}
-type TCPInstance struct {
-	Name                   string `json:"name"`
-	IdleTimeout            int    `json:"idle-timeout"`
-	InsertClientIP         int    `json:"insert-client-ip"`
-	LanFastAck             int    `json:"lan-fast-ack"`
-	ResetFwd               int    `json:"reset-fwd"`
-	ResetRev               int    `json:"reset-rev"`
-	ResetFollowFin         int    `json:"reset-follow-fin"`
-	DelSessionOnServerDown int    `json:"del-session-on-server-down"`
+type SlbTemplateTcp struct {
+	SlbTemplateTCPInstanceName SlbTemplateTCPInstance `json:"tcp,omitempty"`
 }
 
-func GetTcp(id string, name string, host string) (*TCP, error) {
+type SlbTemplateTCPInstance struct {
+	SlbTemplateTCPInstanceAliveIfActive           int    `json:"alive-if-active,omitempty"`
+	SlbTemplateTCPInstanceDelSessionOnServerDown  int    `json:"del-session-on-server-down,omitempty"`
+	SlbTemplateTCPInstanceDisable                 int    `json:"disable,omitempty"`
+	SlbTemplateTCPInstanceDown                    int    `json:"down,omitempty"`
+	SlbTemplateTCPInstanceForceDeleteTimeout      int    `json:"force-delete-timeout,omitempty"`
+	SlbTemplateTCPInstanceForceDeleteTimeout100Ms int    `json:"force-delete-timeout-100ms,omitempty"`
+	SlbTemplateTCPInstanceHalfCloseIdleTimeout    int    `json:"half-close-idle-timeout,omitempty"`
+	SlbTemplateTCPInstanceHalfOpenIdleTimeout     int    `json:"half-open-idle-timeout,omitempty"`
+	SlbTemplateTCPInstanceIdleTimeout             int    `json:"idle-timeout,omitempty"`
+	SlbTemplateTCPInstanceInitialWindowSize       int    `json:"initial-window-size,omitempty"`
+	SlbTemplateTCPInstanceInsertClientIP          int    `json:"insert-client-ip,omitempty"`
+	SlbTemplateTCPInstanceLanFastAck              int    `json:"lan-fast-ack,omitempty"`
+	SlbTemplateTCPInstanceLogging                 string `json:"logging,omitempty"`
+	SlbTemplateTCPInstanceName                    string `json:"name,omitempty"`
+	SlbTemplateTCPInstanceQos                     int    `json:"qos,omitempty"`
+	SlbTemplateTCPInstanceReSelectIfServerDown    int    `json:"re-select-if-server-down,omitempty"`
+	SlbTemplateTCPInstanceResetFollowFin          int    `json:"reset-follow-fin,omitempty"`
+	SlbTemplateTCPInstanceResetFwd                int    `json:"reset-fwd,omitempty"`
+	SlbTemplateTCPInstanceResetRev                int    `json:"reset-rev,omitempty"`
+	SlbTemplateTCPInstanceUUID                    string `json:"uuid,omitempty"`
+	SlbTemplateTCPInstanceUserTag                 string `json:"user-tag,omitempty"`
+}
+
+func PostSlbTemplateTcp(id string, inst SlbTemplateTcp, host string) error {
 
 	logger := util.GetLoggerInstance()
 
@@ -30,33 +43,73 @@ func GetTcp(id string, name string, host string) (*TCP, error) {
 	headers["Accept"] = "application/json"
 	headers["Content-Type"] = "application/json"
 	headers["Authorization"] = id
+	logger.Println("[INFO] Inside PostSlbTemplateTcp")
+	payloadBytes, err := json.Marshal(inst)
+	logger.Println("[INFO] input payload bytes - " + string((payloadBytes)))
+	if err != nil {
+		logger.Println("[INFO] Marshalling failed with error ", err)
+		return err
+	}
 
-	resp, err := DoHttp("GET", "https://"+host+"/axapi/v3/slb/template/tcp/"+name, nil, headers)
+	resp, err := DoHttp("POST", "https://"+host+"/axapi/v3/slb/template/tcp", bytes.NewReader(payloadBytes), headers)
 
 	if err != nil {
-		fmt.Printf("The HTTP request failed with error %s\n", err)
-		logger.Println("The HTTP request failed with error \n", err)
+		logger.Println("The HTTP request failed with error ", err)
+		return err
+	} else {
+		data, _ := ioutil.ReadAll(resp.Body)
+		var m SlbTemplateTcp
+		err := json.Unmarshal(data, &m)
+		if err != nil {
+			logger.Println("Unmarshal error ", err)
+			return err
+		} else {
+			logger.Println("[INFO] Post REQ RES..........................", m)
+			err := check_api_status("PostSlbTemplateTcp", data)
+			if err != nil {
+				return err
+			}
+
+		}
+	}
+	return err
+}
+
+func GetSlbTemplateTcp(id string, name1 string, host string) (*SlbTemplateTcp, error) {
+
+	logger := util.GetLoggerInstance()
+
+	var headers = make(map[string]string)
+	headers["Accept"] = "application/json"
+	headers["Content-Type"] = "application/json"
+	headers["Authorization"] = id
+	logger.Println("[INFO] Inside GetSlbTemplateTcp")
+
+	resp, err := DoHttp("GET", "https://"+host+"/axapi/v3/slb/template/tcp/"+name1, nil, headers)
+
+	if err != nil {
+		logger.Println("The HTTP request failed with error ", err)
 		return nil, err
 	} else {
 		data, _ := ioutil.ReadAll(resp.Body)
-		var m TCP
-		erro := json.Unmarshal(data, &m)
-		if erro != nil {
-			fmt.Printf("Unmarshal error %s\n", err)
+		var m SlbTemplateTcp
+		err := json.Unmarshal(data, &m)
+		if err != nil {
+			logger.Println("Unmarshal error ", err)
 			return nil, err
 		} else {
-			fmt.Print(m)
-			logger.Println("[INFO] GET REQ RES..........................", m)
-			err := check_api_status("GetTcp", data)
+			logger.Println("[INFO] Get REQ RES..........................", m)
+			err := check_api_status("GetSlbTemplateTcp", data)
 			if err != nil {
 				return nil, err
 			}
 			return &m, nil
 		}
 	}
+
 }
 
-func PostTcp(id string, sg TCP, host string) error {
+func PutSlbTemplateTcp(id string, name1 string, inst SlbTemplateTcp, host string) error {
 
 	logger := util.GetLoggerInstance()
 
@@ -64,102 +117,68 @@ func PostTcp(id string, sg TCP, host string) error {
 	headers["Accept"] = "application/json"
 	headers["Content-Type"] = "application/json"
 	headers["Authorization"] = id
-
-	payloadBytes, err := json.Marshal(sg)
-
+	logger.Println("[INFO] Inside PutSlbTemplateTcp")
+	payloadBytes, err := json.Marshal(inst)
 	logger.Println("[INFO] input payload bytes - " + string((payloadBytes)))
-
 	if err != nil {
-		logger.Println("[INFO] Marshalling failed with error \n", err)
+		logger.Println("[INFO] Marshalling failed with error ", err)
+		return err
 	}
-	resp, err := DoHttp("POST", "https://"+host+"/axapi/v3/slb/template/tcp", bytes.NewReader(payloadBytes), headers)
+
+	resp, err := DoHttp("PUT", "https://"+host+"/axapi/v3/slb/template/tcp/"+name1, bytes.NewReader(payloadBytes), headers)
 
 	if err != nil {
-		fmt.Printf("The HTTP request failed with error %s\n", err)
-		logger.Println("The HTTP request failed with error \n", err)
-	} else {
-		data, _ := ioutil.ReadAll(resp.Body)
-		var m TCP
-		erro := json.Unmarshal(data, &m)
-		if erro != nil {
-			fmt.Printf("Unmarshal error %s\n", err)
-		} else {
-			fmt.Println("response Body:", string(data))
-			logger.Println("response Body:", string(data))
-			err := check_api_status("PostTcp", data)
-			if err != nil {
-				return err
-			}
-		}
-	}
-	return err
-}
-
-func PutTcp(id string, name string, sg TCP, host string) error {
-
-	logger := util.GetLoggerInstance()
-
-	var headers = make(map[string]string)
-	headers["Accept"] = "application/json"
-	headers["Content-Type"] = "application/json"
-	headers["Authorization"] = id
-
-	payloadBytes, err := json.Marshal(sg)
-
-	logger.Println("[INFO] input payload bytes - " + string((payloadBytes)))
-
-	if err != nil {
-		logger.Println("[INFO] Marshalling failed with error \n", err)
-	}
-	resp, err := DoHttp("PUT", "https://"+host+"/axapi/v3/slb/template/tcp/"+name, bytes.NewReader(payloadBytes), headers)
-
-	if err != nil {
-		fmt.Printf("The HTTP request failed with error %s\n", err)
-		logger.Println("The HTTP request failed with error \n", err)
-	} else {
-		data, _ := ioutil.ReadAll(resp.Body)
-		var m TCP
-		erro := json.Unmarshal(data, &m)
-		if erro != nil {
-			fmt.Printf("Unmarshal error %s\n", err)
-		} else {
-			fmt.Println("response Body:", string(data))
-			logger.Println("response Body:", string(data))
-			err := check_api_status("PutTcp", data)
-			if err != nil {
-				return err
-			}
-		}
-	}
-	return err
-}
-
-func DeleteTcp(id string, name string, host string) error {
-
-	logger := util.GetLoggerInstance()
-
-	var headers = make(map[string]string)
-	headers["Accept"] = "application/json"
-	headers["Content-Type"] = "application/json"
-	headers["Authorization"] = id
-
-	resp, err := DoHttp("DELETE", "https://"+host+"/axapi/v3/slb/template/tcp/"+name, nil, headers)
-
-	if err != nil {
-		fmt.Printf("The HTTP request failed with error %s\n", err)
-		logger.Println("The HTTP request failed with error \n", err)
+		logger.Println("The HTTP request failed with error ", err)
 		return err
 	} else {
 		data, _ := ioutil.ReadAll(resp.Body)
-		var m TCP
-		erro := json.Unmarshal(data, &m)
-		if erro != nil {
-			fmt.Printf("Unmarshal error %s\n", err)
+		var m SlbTemplateTcp
+		err := json.Unmarshal(data, &m)
+		if err != nil {
+			logger.Println("Unmarshal error ", err)
 			return err
 		} else {
-			fmt.Print(m)
-			logger.Println("[INFO] GET REQ RES..........................", m)
+			logger.Println("[INFO] Put REQ RES..........................", m)
+			err := check_api_status("PutSlbTemplateTcp", data)
+			if err != nil {
+				return err
+			}
+
 		}
 	}
-	return nil
+	return err
+}
+
+func DeleteSlbTemplateTcp(id string, name1 string, host string) error {
+
+	logger := util.GetLoggerInstance()
+
+	var headers = make(map[string]string)
+	headers["Accept"] = "application/json"
+	headers["Content-Type"] = "application/json"
+	headers["Authorization"] = id
+	logger.Println("[INFO] Inside DeleteSlbTemplateTcp")
+
+	resp, err := DoHttp("DELETE", "https://"+host+"/axapi/v3/slb/template/tcp/"+name1, nil, headers)
+
+	if err != nil {
+		logger.Println("The HTTP request failed with error ", err)
+		return err
+	} else {
+		data, _ := ioutil.ReadAll(resp.Body)
+		var m SlbTemplateTcp
+		err := json.Unmarshal(data, &m)
+		if err != nil {
+			logger.Println("Unmarshal error ", err)
+			return err
+		} else {
+			logger.Println("[INFO] Delete REQ RES..........................", m)
+			err := check_api_status("DeleteSlbTemplateTcp", data)
+			if err != nil {
+				return err
+			}
+
+		}
+	}
+	return err
 }
--- website/docs/r/thunder_slb_template_tcp.html.markdown
+++ website/docs/r/thunder_slb_template_tcp.html.markdown
@@ -3,7 +3,7 @@ layout: "thunder"
 page_title: "thunder: thunder_slb_template_tcp"
 sidebar_current: "docs-thunder-resource-slb-template-tcp"
 description: |-
-	Provides details about thunder slb template tcp resource for A10
+    Provides details about thunder slb template tcp resource for A10
 ---
 
 # thunder\_slb\_template\_tcp
@@ -14,56 +14,61 @@ description: |-
 
 ```hcl
 provider "thunder" {
-    address  = "${var.address}"
-    username = "${var.username}"  
-    password = "${var.password}"
+  address  = var.address
+  username = var.username
+  password = var.password
 }
 
-resource "thunder_slb_template_tcp" "Slb_Template_Tcp_Test" {
 
-del_session_on_server_down = 0
-initial_window_size = 0
-half_open_idle_timeout = 0
+resource "thunder_slb_template_tcp" "resourceSlbTemplateTcpTest" {
+	name = "string"
 logging = "string"
-name = "string"
-reset_fwd = 0
-reset_follow_fin = 0
-alive_if_active = 0
 idle_timeout = 0
+half_open_idle_timeout = 0
+half_close_idle_timeout = 0
+initial_window_size = 0
 force_delete_timeout = 0
-user_tag = "string"
-down = 0
-disable = 0
-reset_rev = 0
+force_delete_timeout_100ms = 0
+alive_if_active = 0
+qos = 0
 insert_client_ip = 0
 lan_fast_ack = 0
-half_close_idle_timeout = 0
-force_delete_timeout_100ms = 0
-qos = 0
+reset_fwd = 0
+reset_rev = 0
+reset_follow_fin = 0
+disable = 0
+down = 0
+re_select_if_server_down = 0
+del_session_on_server_down = 0
 uuid = "string"
+user_tag = "string"
  
 }
+
 ```
 
 ## Argument Reference
 
-* `alive_if_active` - keep connection alive if active traffic
-* `del_session_on_server_down` - Delete session if the server/port goes down (either disabled/hm down)
+* `tcp` - L4 TCP switch config
+* `name` - Fast TCP Template Name
+* `logging` - 'init': init only log; 'term': termination only log; 'both': both initial and termination log;
+* `idle-timeout` - Idle Timeout value (Interval of 60 seconds), default 120 seconds (idle timeout in second, default 120)
+* `half-open-idle-timeout` - TCP Half Open Idle Timeout (sec), default off (half open idle timeout in second, default off)
+* `half-close-idle-timeout` - TCP Half Close Idle Timeout (sec), default off (half close idle timeout in second, default off)
+* `initial-window-size` - Set the initial window size (number)
+* `force-delete-timeout` - The maximum time that a session can stay in the system before being delete (number (second))
+* `force-delete-timeout-100ms` - The maximum time that a session can stay in the system before being delete (number in 100ms)
+* `alive-if-active` - keep connection alive if active traffic
+* `qos` - QOS level (number)
+* `insert-client-ip` - Insert client ip into TCP option
+* `lan-fast-ack` - Enable fast TCP ack on LAN
+* `reset-fwd` - send reset to server if error happens
+* `reset-rev` - send reset to client if error happens
+* `reset-follow-fin` - send reset to client or server upon receiving first fin
 * `disable` - send reset to client when server is disabled
 * `down` - send reset to client when server is down
-* `force_delete_timeout` - The maximum time that a session can stay in the system before being delete (number (second))
-* `force_delete_timeout_100ms` - The maximum time that a session can stay in the system before being delete (number in 100ms)
-* `half_close_idle_timeout` - TCP Half Close Idle Timeout (sec), default off (half close idle timeout in second, default off)
-* `half_open_idle_timeout` - TCP Half Open Idle Timeout (sec), default off (half open idle timeout in second, default off)
-* `idle_timeout` - Idle Timeout value (Interval of 60 seconds), default 120 seconds (idle timeout in second, default 120)
-* `initial_window_size` - Set the initial window size (number)
-* `insert_client_ip` - Insert client ip into TCP option
-* `lan_fast_ack` - Enable fast TCP ack on LAN
-* `logging` - ‘init’: init only log; ‘term’: termination only log; ‘both’: both initial and termination log;
-* `name` - Fast TCP Template Name
-* `qos` - QOS level (number)
-* `reset_follow_fin` - send reset to client or server upon receiving first fin
-* `reset_fwd` - send reset to server if error happens
-* `reset_rev` - send reset to client if error happens
-* `user_tag` - Customized tag
+* `re-select-if-server-down` - re-select another server if service port is down
+* `del-session-on-server-down` - Delete session if the server/port goes down (either disabled/hm down)
 * `uuid` - uuid of the object
+* `user-tag` - Customized tag
+
